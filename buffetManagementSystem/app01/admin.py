c59359f6d74dc9f42039b0fafd90a23524513303
Yuanyuan <zky5@txstate.edu>
2025-04-14 13:24:45 -0500

    add admin

--- buffetManagementSystem/app01/admin.py
+++ buffetManagementSystem/app01/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Department, UserInfo, Price, Order;
+
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(UserInfo)
+admin.site.register(Price)
+admin.site.register(Order)
--- buffetManagementSystem/buffetManagementSystem/urls.py
+++ buffetManagementSystem/buffetManagementSystem/urls.py
@@ -17,9 +17,10 @@ Including another URLconf
 # from django.contrib import admin
 from django.urls import path
 from app01 import views
+from django.contrib import admin
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('depart/list/', views.depart_list),
     path('depart/add/', views.depart_add),
 
