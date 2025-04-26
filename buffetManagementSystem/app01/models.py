59f6e9eaa60df6102f8b262200dce5c60beeca2a
Xiaohui <bqd12@txstate.edu>
2025-04-25 20:16:39 -0500

    Add Statistics part

--- buffetManagementSystem/app01/models.py
+++ buffetManagementSystem/app01/models.py
@@ -20,23 +20,7 @@ class UserInfo(models.Model):
     password = models.CharField(verbose_name='Password', max_length=64)
     age = models.IntegerField(verbose_name='Age')
     account = models.DecimalField(verbose_name='Account', decimal_places=2, max_digits=10, default=0)
-
-
-    # 无约束
-    # depart_id = models。BigIntegerField(verbose_name="Department')
-
-    # 1.有约束
-    #  -to， 与哪张表关联
-    #  -to_field, 表中哪一列关联
-    # 2. django 自动
-    #  -写的depart
-    #  -生成数据列 depart_id
-    # 3. 部门表被删除
-    # 3.1 级联删除
     department = models.ForeignKey(to="Department", to_field="id", on_delete=models.CASCADE)
-    # 3.2 置空
-    #depart = models.ForeignKey(to="Department", to_field="id", null=True, blank=True, on_delete=models.SET。NULL)
-
     gender_choices = (
         (1, 'male'),
         (2, 'female'),
